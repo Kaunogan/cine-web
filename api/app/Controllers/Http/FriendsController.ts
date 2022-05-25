a2005b39fab7111ee3be5348e749fc1de6213851
GUMBAUElric <elric.gumbau@ynov.com>
2022-05-26 01:38:31 +0200

    feat(User): create route to see specific user friend

--- api/app/Controllers/Http/FriendsController.ts
+++ api/app/Controllers/Http/FriendsController.ts
@@ -3,27 +3,56 @@ import User from 'App/Models/User'
 import AddUserFriendValidator from 'App/Validators/AddUserFriendValidator'
 
 export default class FriendsController {
-  public async index({ auth, bouncer, request }: HttpContextContract) {
+  public async index({ bouncer, request }: HttpContextContract) {
     const id = request.param('user_id')
     const page = request.input('page', 1)
     const limit = request.input('limit', 12)
 
     await bouncer.with('UserPolicy').authorize('view', id)
 
-    return auth.user
-      ?.related('friends')
+    const user = await User.findOrFail(id)
+
+    return user
+      .related('friends')
       .query()
       .wherePivot('user_id', id)
       .select(['id', 'email', 'pseudo'])
       .paginate(page, limit)
   }
 
-  public async store({ auth, bouncer, request, response }: HttpContextContract) {
+  public async show({ bouncer, request, response }: HttpContextContract) {
+    const userId = request.param('user_id')
+    let friendId = request.param('id')
+
+    friendId = friendId === 0 ? 0 : friendId - 1
+
+    await bouncer.with('UserPolicy').authorize('view', userId)
+
+    const user = await User.findOrFail(userId)
+
+    const userFriend = await user
+      .related('friends')
+      .query()
+      .wherePivot('user_id', userId)
+      .select(['id', 'email', 'pseudo'])
+
+    if (friendId >= userFriend.length) {
+      return response.status(404).send({
+        message: 'Friend not found',
+      })
+    }
+
+    return userFriend[friendId]
+  }
+
+  public async store({ bouncer, request, response }: HttpContextContract) {
     const id = request.param('user_id')
     const { friend } = await request.validate(AddUserFriendValidator)
     const userFriend = await User.findOrFail(friend)
 
     await bouncer.with('UserPolicy').authorize('view', id)
+
+    const user = await User.findOrFail(id)
 
     if (id === userFriend.id) {
       return response.status(400).send({
@@ -31,8 +60,8 @@ export default class FriendsController {
       })
     }
 
-    const friendAlreadyExist = await auth.user
-      ?.related('friends')
+    const friendAlreadyExist = await user
+      .related('friends')
       .query()
       .wherePivot('user_id', id)
       .where('friend', userFriend.id)
@@ -43,7 +72,7 @@ export default class FriendsController {
       })
     }
 
-    await auth.user?.related('friends').attach([userFriend.id])
+    await user.related('friends').attach([userFriend.id])
 
     return {
       message: 'ok',
