fac1b1b990105e4465e75e88f206d64b0ab73d43
GUMBAUElric <elric.gumbau@ynov.com>
2022-05-30 14:11:27 +0200

    feat(interface): create film interfaces

---
+++ api/services/tmdb/interfaces.ts
@@ -0,0 +1,41 @@
+/*
+|--------------------------------------------------------------------------
+| Movie Interface
+|--------------------------------------------------------------------------
+|
+| Movie Interface contains the information of the film displayed in the lists
+|
+| tmdbMovieId : the id of the movie from the TMDB api
+| title: the title of the movie
+| posterUrl: poster url of the movie
+|
+*/
+interface IMovie {
+  tmdbMovieId: number
+  title: string
+  posterUrl: string
+}
+
+/*
+|--------------------------------------------------------------------------
+| Movie Details Interface
+|--------------------------------------------------------------------------
+|
+| Movie Details Interface contains the details information of the searched movie
+|
+| tmdbMovieId : the id of the movie from the TMDB api
+| title: the title of the movie
+| overview: synopsis of the movie
+| backdropUrl: background of the movie
+| trailerUrl: The youtube embed trailer url
+|
+*/
+interface IMovieDetails {
+  tmdbMovieId: number
+  title: string
+  overview: string
+  backdropUrl: string
+  trailerUrl: string
+}
+
+export { IMovie, IMovieDetails }
