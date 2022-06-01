2faf92e621b0609d408d1c5abdb2ba0c08bc2c75
GUMBAUElric <elric.gumbau@ynov.com>
2022-06-01 11:19:26 +0200

    feat(swagger): comment swagger config

--- api/config/swagger.ts
+++ api/config/swagger.ts
@@ -1,16 +1,82 @@
 import { SwaggerConfig } from '@ioc:Adonis/Addons/Swagger'
 
-export default {
-  uiEnabled: true, // disable or enable swaggerUi route
-  uiUrl: '/api/docs', // url path to swaggerUI
-  specEnabled: true, // disable or enable swagger.json route
+const config = {
+  /*
+  |--------------------------------------------------------------------------
+  | uiEnabled
+  |--------------------------------------------------------------------------
+  |
+  | A boolean disable or enable swaggerUi route
+  |
+  | Setting the value to `true` will enable the swagger ui.
+  |
+  */
+  uiEnabled: true,
+
+  /*
+  |--------------------------------------------------------------------------
+  | uiUrl
+  |--------------------------------------------------------------------------
+  |
+  | A string to set url path to swaggerUI
+  |
+  */
+  uiUrl: '/api/docs',
+
+  /*
+  |--------------------------------------------------------------------------
+  | specEnabled
+  |--------------------------------------------------------------------------
+  |
+  | A boolean disable or enable swagger.json route
+  |
+  | Setting the value to `true` will load the Open API specification
+  |
+  */
+  specEnabled: true,
+
+  /*
+  |--------------------------------------------------------------------------
+  | specUrl
+  |--------------------------------------------------------------------------
+  |
+  | A boolean disable or enable swagger.json route
+  |
+  | Setting the value to `true` will load the Open API specification
+  |
+  */
   specUrl: '/swagger.json',
 
-  middleware: [], // middlewares array, for protect your swagger docs and spec endpoints
+  /*
+  |--------------------------------------------------------------------------
+  | middleware
+  |--------------------------------------------------------------------------
+  |
+  | An array of middlewares for protect your swagger docs and spec endpoints
+  |
+  */
+  middleware: [],
 
   options: {
     definition: {
+      /*
+      |--------------------------------------------------------------------------
+      | openapi
+      |--------------------------------------------------------------------------
+      |
+      | The version of Open API used
+      |
+      */
       openapi: '3.0.0',
+
+      /*
+      |--------------------------------------------------------------------------
+      | info
+      |--------------------------------------------------------------------------
+      |
+      | The info about the application
+      |
+      */
       info: {
         title: 'CineWeb swagger docs',
         version: '1.0.0',
@@ -18,6 +84,14 @@ export default {
       },
       components: {
         securitySchemes: {
+          /*
+          |--------------------------------------------------------------------------
+          | bearerAuth
+          |--------------------------------------------------------------------------
+          |
+          | The bearer auth used inside the application
+          |
+          */
           bearerAuth: {
             type: 'http',
             scheme: 'bearer',
@@ -26,9 +100,46 @@ export default {
       },
     },
 
+    /*
+    |--------------------------------------------------------------------------
+    | apis
+    |--------------------------------------------------------------------------
+    |
+    | Array of files application used for Swagger
+    |
+    */
     apis: ['app/**/*.ts', 'docs/swagger/**/*.yml', 'start/routes.ts'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | basePath
+    |--------------------------------------------------------------------------
+    |
+    | The basePath used by Swagger
+    |
+    */
     basePath: '/',
   },
+
+  /*
+  |--------------------------------------------------------------------------
+  | mode
+  |--------------------------------------------------------------------------
+  |
+  | The mode used by Swagger depending on NODE_ENV
+  |
+  */
   mode: process.env.NODE_ENV === 'production' ? 'PRODUCTION' : 'RUNTIME',
+
+  /*
+  |--------------------------------------------------------------------------
+  | specFilePath
+  |--------------------------------------------------------------------------
+  |
+  | The path to the specification file used when is in production
+  |
+  */
   specFilePath: 'docs/swagger.json',
 } as SwaggerConfig
+
+export default config
