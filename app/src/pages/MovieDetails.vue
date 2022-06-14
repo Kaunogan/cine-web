d04ae8f3a437955b23ffc5e88f14e48a215dc7bc
GUMBAUElric <elric.gumbau@ynov.com>
2022-06-15 00:40:20 +0200

    feat(movieDetails): add movie trailer

--- app/src/pages/MovieDetails.vue
+++ app/src/pages/MovieDetails.vue
@@ -7,6 +7,13 @@
         <p class="movie-overview">{{ movie.overview }}</p>
       </div>
     </div>
+    <div class="cw-movie-details-container__content">
+      <div class="cw-movie-details-container__content-trailer">
+        <h2 class="max-w-screen-2xl text-center font-header text-2xl font-light text-tertiary md:text-3xl">Trailer</h2>
+        <iframe v-if="movie.trailer_url" :src="movie.trailer_url" class="trailer" allowfullscreen></iframe>
+        <h2 v-else class="text-xl font-thin text-primary md:text-2xl">No trailer</h2>
+      </div>
+    </div>
   </div>
   <div v-else class="flex h-full w-full items-center justify-center">
     <cw-loader />
@@ -18,22 +25,27 @@ import { useRoute } from 'vue-router'
 import { onMounted, ref } from 'vue'
 import * as MovieService from '@/services/Movies'
 import CwLoader from '@/components/cwLoader.vue'
+import { IMovie } from '@/types'
 
 const route = useRoute()
 const tmdbMovieId = Number(route.params.id)
-const movie = ref()
+const movie = ref<IMovie.Details>()
 
+// Function
 onMounted(async () => {
   movie.value = await MovieService.getMoviesDetails(tmdbMovieId)
+
+  movie.value.backdrop_url = movie.value.backdrop_url.includes('null') ? 'https://pbs.twimg.com/profile_images/527991807402328064/LmS_7fXc_400x400.jpeg' : movie.value.backdrop_url
+  movie.value.overview = movie.value.overview !== '' ? movie.value?.overview : 'No overview for this movie'
 })
 </script>
 
 <style lang="scss" scoped>
 .cw-movie-details-container {
-  @apply h-full flex-1 bg-white;
+  @apply flex flex-1 flex-col bg-secondary bg-white;
 
   &__header {
-    @apply relative h-72  w-full md:h-1/3;
+    @apply relative h-72 w-full md:h-1/3;
     background-color: black;
     box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
   }
@@ -41,10 +53,18 @@ onMounted(async () => {
   &__header-content {
     @apply absolute top-0 left-0 flex h-full w-full flex-col items-center justify-evenly px-6 md:px-12 2xl:px-48;
   }
+
+  &__content {
+    @apply flex h-full w-full items-center justify-center px-4 lg:px-0;
+  }
+
+  &__content-trailer {
+    @apply flex h-3/4 w-full max-w-screen-sm flex-col items-center justify-evenly px-4 3xl:max-w-screen-lg;
+  }
 }
 
 .backdrop {
-  @apply h-full w-full object-cover object-left-top opacity-40;
+  @apply h-full w-full object-cover object-center opacity-40;
 }
 
 .movie-title {
@@ -52,9 +72,13 @@ onMounted(async () => {
 }
 
 .movie-overview {
-  @apply w-full max-w-screen-2xl overflow-hidden text-center text-white md:text-lg;
+  @apply w-full max-w-screen-2xl overflow-hidden text-center font-light text-white md:text-lg;
   display: -webkit-box;
-  -webkit-line-clamp: 6;
+  -webkit-line-clamp: 4;
   -webkit-box-orient: vertical;
+}
+
+.trailer {
+  @apply h-3/4 w-full;
 }
 </style>
