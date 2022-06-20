e4c45110a4d24f2d34db5a6aeffcb7f2738b4936
GUMBAUElric <elric.gumbau@ynov.com>
2022-06-20 22:26:30 +0200

    feat(friendsPage): add function to delete friend user

--- app/src/pages/friends/Friends.vue
+++ app/src/pages/friends/Friends.vue
@@ -1,14 +1,28 @@
 <template>
-  <cw-container>
+  <cw-container class="relative">
+    <cw-modal :show-modal="state.showModal">
+      <div class="cw-g-card cw-category-details-modal-content">
+        <h1 class="font-header text-xl text-tertiary">Are you sure ?</h1>
+        <div class="mt-6 flex items-center justify-center">
+          <cw-button btn-type="primary" @click="changeModalVisibility">Cancel</cw-button>
+          <cw-button btn-type="danger" class="ml-6" @click="deleteUser">Delete</cw-button>
+        </div>
+      </div>
+    </cw-modal>
+
     <cw-container-nav-bar>
       <h2 class="hidden font-header text-2xl font-light text-tertiary lg:block">Friends</h2>
       <ph-list size="28" class="lg:hidden" @click="components.slideSideBar()" />
-      <cw-settings-dropdown />
+      <div class="flex h-full w-full items-center justify-end lg:w-1/2 lg:justify-end">
+        <ph-pencil size="30" class="mr-6 cursor-pointer transition duration-300 ease-in-out" :class="state.isDeleteMode ? 'text-secondary' : 'text-primary'" @click="changeDeleteMode" />
+        <cw-settings-dropdown />
+      </div>
     </cw-container-nav-bar>
 
     <cw-container-content class="flex flex-col items-center justify-evenly md:h-full">
       <cw-grid-list :is-loading="state.isLoading" :evenly="state.isEvenly" :nb-of-rows="3" :centered="currentFriendsIsEmpty" msg-empty-data="No Friends found">
-        <div v-for="friend in state.currentFriends" :key="friend.id" class="cw-friends-card">
+        <div v-for="friend in state.currentFriends" :key="friend.id" class="cw-friends-card z-10">
+          <ph-trash v-if="state.isDeleteMode" size="28" class="absolute top-1 left-1 z-0 cursor-pointer text-secondary" @click="changeModalVisibility(friend.id)" />
           <p class="text-center lg:text-lg">{{ friend.pseudo }}</p>
         </div>
       </cw-grid-list>
@@ -37,10 +51,14 @@ import CwButton from '@/components/cwButton.vue'
 import CwPagination from '@/components/cwPagination.vue'
 import { useRouter } from 'vue-router'
 import { breakpointsTailwind, useBreakpoints, useWindowSize } from '@vueuse/core'
+import { useToast } from 'vue-toastification'
+import CwModal from '@/components/cwModal.vue'
 
 const components = useComponents()
 const { width } = useWindowSize()
+const toast = useToast()
 const breakpoints = useBreakpoints(breakpointsTailwind)
+let userFriendIdToDelete = 0
 
 // State
 const state = reactive({
@@ -49,6 +67,8 @@ const state = reactive({
   nextFriends: <IFriendUser[]>[],
   isLoading: true,
   isEvenly: false,
+  isDeleteMode: false,
+  showModal: false,
 })
 
 const router = useRouter()
@@ -83,6 +103,27 @@ const goToAddFriend = async () => {
   await router.push({ path: '/friends/add' })
 }
 
+const changeDeleteMode = () => {
+  state.isDeleteMode = !state.isDeleteMode
+}
+
+const changeModalVisibility = (friendId: number = -1) => {
+  userFriendIdToDelete = friendId
+  state.showModal = !state.showModal
+}
+
+const deleteUser = async () => {
+  const message = await FriendService.removeFriend(userFriendIdToDelete)
+
+  state.currentPage = 1
+  state.currentFriends = await FriendService.getFriends(state.currentPage)
+  state.nextFriends = await FriendService.getFriends(state.currentPage + 1)
+  state.isEvenly = state.currentFriends.length <= 6 && state.currentFriends.length !== 0
+
+  changeModalVisibility()
+  toast.success(message)
+}
+
 onMounted(async () => {
   state.currentFriends = await FriendService.getFriends(state.currentPage)
   state.nextFriends = await FriendService.getFriends(state.currentPage + 1)
@@ -93,6 +134,6 @@ onMounted(async () => {
 
 <style lang="scss" scoped>
 .cw-friends-card {
-  @apply flex h-32 w-36 items-center justify-center rounded-2xl border border-tertiary 2xl:w-48;
+  @apply relative flex h-32 w-36 items-center justify-center rounded-2xl border border-tertiary 2xl:w-48;
 }
 </style>
--- app/src/services/Friend.ts
+++ app/src/services/Friend.ts
@@ -2,7 +2,7 @@ import useAuth from '@/stores/authStore'
 import HttpController from '@/controllers/Http'
 import { IFriendUser } from '@/types'
 
-export async function getFriends(page: number = 1, limit: number = 12) {
+export async function getFriends(page: number = 1, limit: number = 8) {
   const auth = useAuth()
   const httpController = new HttpController(`/users/${auth.userId}`)
 
@@ -16,6 +16,15 @@ export async function addFriend(friendEmail: { friend_email: string }) {
   const httpController = new HttpController(`/users/${auth.userId}`)
 
   const { message } = await httpController.post('/friends', friendEmail, { Authorization: `Bearer ${auth.token}` })
+
+  return message
+}
+
+export async function removeFriend(friendId: number) {
+  const auth = useAuth()
+  const httpController = new HttpController(`/users/${auth.userId}`)
+
+  const { message } = await httpController.delete(`/friends/${friendId}`, { Authorization: `Bearer ${auth.token}` })
 
   return message
 }
